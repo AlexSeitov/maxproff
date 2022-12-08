8276a8eeb5cb00be9d04d829b3b4dd7da080543a
AlexSeitov <alexander.seitov@yandex.ru>
2022-12-08 17:35:19 +0300

    fixed errors

--- src/scripts/modules/form.js
+++ src/scripts/modules/form.js
@@ -3,6 +3,8 @@ export default function form(formEl) {
 
   calculateForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    // const data = Object.fromEntries(new FormData(e.target));
+    const data = Object.fromEntries(new FormData(e.target));
+    // eslint-disable-next-line no-console
+    console.log(data);
   });
 }
